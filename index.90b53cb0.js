354cd2ca9e0246cda9f43a4eada82051bac40112
ElenaOds <106108433+ElenaOds@users.noreply.github.com>
2024-02-23 16:28:13 +0000

    Deploying to gh-pages from @ ElenaOds/flowers@5673d3a1636eb9b95e03f147da8cd29bf9451e0f 🚀

---
+++ index.90b53cb0.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".carousel").slick({dots:!1,infinite:!0,speed:300,swipeToSlide:!0,autoplay:!0,autoplaySpeed:7e3,slidesToShow:2.98,slidesToScroll:1,arrows:!0,prevArrow:$(".prev"),nextArrow:$(".next"),responsive:[{breakpoint:768,settings:{slidesToShow:2,slidesToScroll:1}}]})}));
+//# sourceMappingURL=index.90b53cb0.js.map
