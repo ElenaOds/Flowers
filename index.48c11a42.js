6863f3f28244fb4218c31d34241dbedb3ca2d08c
ElenaOds <106108433+ElenaOds@users.noreply.github.com>
2024-02-28 17:04:03 +0000

    Deploying to gh-pages from @ ElenaOds/flowers@7214df6a25590b430ee7d7dbcc5c538fcd7b11df 🚀

---
+++ index.48c11a42.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector(".form-input"),e=document.querySelector(".form-button");t.addEventListener("input",(function(){t.classList.contains("valid")?e.disabled=!1:e.disabled=!0}))}();
+//# sourceMappingURL=index.48c11a42.js.map
