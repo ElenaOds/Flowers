cdd8ba369dcd14e262549cb7cec4b1e455ab81fa
Elena Pedko <elpg.ods@gmail.com>
2024-02-29 15:23:10 +0200

    logic for subscribe form is added

--- src/js/form.js
+++ src/js/form.js
@@ -1,18 +1,47 @@
 (() => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
-    const input = document.querySelector(".form-input");
-    const submitButton = document.querySelector(".form-button");
-    
+    const subscribeForm = document.querySelector('.js-subscribe-form');
+    const input = document.querySelector('.js-subscribe-input');
+    const button = document.querySelector('.js-subscribe-button');
 
-    input.addEventListener("input", () => {
-      
-        if (input.classList.contains("valid")) {
-          
-            submitButton.disabled = false;
-        } else {
-     
-            submitButton.disabled = true;
+    subscribeForm.addEventListener('submit', onFormSubmit);
+    input.addEventListener('input', onInputChange);
+    input.addEventListener('input', onInputBlur);
+
+
+    function onInputBlur(event) {
+        if(event.currentTarget.value === '') {
+        input.classList.remove('valid')
+        
+        input.classList.remove('invalid');
         }
-      });
-   
+    }
+
+    function onFormSubmit(event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+        console.log(formData);
+        
+        event.currentTarget.reset();
+        button.disabled = true;
+        input.classList.remove('valid');
+        input.classList.remove('invalid');
+    }
+
+    function onInputChange(event) {
+        input.classList.remove('valid');
+        input.classList.remove('invalid');
+
+        if (event.currentTarget.value.match(emailRegex)) {
+
+            button.disabled = false;
+            input.classList.add('valid')
+        } else {
+          
+            button.disabled = true;
+            input.classList.add('invalid')
+        }
+    }
 })();
